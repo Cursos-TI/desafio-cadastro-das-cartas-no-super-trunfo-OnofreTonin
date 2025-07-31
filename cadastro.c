20ffa299b8c5de04c8af0a3201d9805f69f9ee2e
OnofreTonin <nonofrej33@gmail.com>
2025-07-31 15:51:09 -0300

    Desafio Nível Mestre - Comparações

--- cadastro.c
+++ cadastro.c
@@ -15,9 +15,10 @@ int main (){
     while (getchar() != '\n'); // limpando buffer
     fgets(city, 30, stdin);
 
-    int population;
+    unsigned long int population;
     printf("Diga quantas pessoas tem:\n");
-    scanf(" %d", &population);
+    scanf(" %u", &population);
+
 
     float area;
     printf("Qual é a area dessa cidade?\n");
@@ -36,16 +37,19 @@ int main (){
     densidade = (float) population/area; //fazendo o calculo de pibpercapt fazendo o cast das váriaveis population e area
     float pibpercapt;
     pibpercapt = (float) pib/population; //fazendo o calculo de pibpercapt fazendo o cast das váriaveis pib e population
+    float Superpoder;
+    Superpoder = (population + area + pib + numtur +  pibpercapt) / densidade;
     printf("*¨¨Carta 01¨¨*\n");
     printf("Estado %c\n", state);
     printf("Código %s\n", code);
     printf("Nome da cidade %s", city);
-    printf("População %d\n", population);
-    printf("Área %.3f\n", area);
-    printf("PIB:%.3f\n", pib);
+    printf("População %u\n", population);
+    printf("Área %.2f\n", area);
+    printf("PIB:%.2f\n", pib);
     printf("Pontos túristicos: %d\n", numtur);
     printf("Densidade Populacional %.1f  hab/km²\n", densidade);
     printf("Pib perCapita: %.2f\n", pibpercapt);
+    printf("Super Poder %.2f\n", Superpoder);
 
     char state2;
     printf("De A a H escolha uma letra\n");
@@ -60,9 +64,9 @@ int main (){
     while (getchar() != '\n'); // limpando buffer
     fgets(city2, 30, stdin);
 
-    int population2;
+    unsigned long int population2;
     printf("Diga quantas pessoas tem:\n");
-    scanf(" %d", &population2);
+    scanf(" %u", &population2);
 
     float area2;
     printf("Qual é a area dessa cidade?\n");
@@ -80,17 +84,46 @@ int main (){
     densidade2 = (float) population2/area2; //fazendo o calculo de pibpercapt fazendo o cast das váriaveis population2 e area2
     float pibpercapt2;
     pibpercapt2 = (float) pib2/population2; //fazendo o calculo de pibpercapt fazendo o cast das váriaveis pib2 e population2
+    float Superpoder2;
+    Superpoder2 = (population2 + area2 + pib2 + numtur2 +  pibpercapt2) / densidade2;
     printf("*¨¨Carta 02¨¨*\n");
     printf("Estado %c\n", state2);
     printf("Código %s\n", code2);
     printf("Nome da cidade %s", city2);
-    printf("População %d\n", population2);
+    printf("População %u\n", population2);
     printf("Área %f\n", area2);
     printf("PIB:%f\n", pib2);
     printf("Pontos túristicos: %d\n", numtur2);
     printf("Densidade Populacional %.1f  hab/km²\n", densidade2);
     printf("Pib perCapita: %.2f\n", pibpercapt2);
+    printf("Super Poder %f\n", Superpoder2);
 
+    /*Modelo de saída
+    Comparação de Cartas:
+
+    População: Carta 1 venceu (1)a
+
+    Área: Carta 1 venceu (1)
+
+    PIB: Carta 1 venceu (1)
+
+    Pontos Turísticos: Carta 1 venceu (1)
+
+    Densidade Populacional: Carta 2 venceu (0)
+
+    PIB per Capita: Carta 1 venceu (1)
+
+    Super Poder: Carta 1 venceu (1)
+    */
+
+    printf("*¨¨Comparação de Cartas¨¨*\n");
+    printf("População: Carta 1 venceu (%d)\n", population>population2);
+    printf("Área: Carta 1 venceu(%d)\n", area>area2);
+    printf("PIB: Carta 1 venceu (%d)\n", pib>pib2);
+    printf("Pontos Turísticos: Carta 1 venceu(%d)\n", numtur>numtur2);
+    printf("Densidade Populacional: Carta 2 venceu(%d)\n", densidade2>densidade);
+    printf("PIB per Capita: Carta 1 venceu(%d)\n", pibpercapt>pibpercapt2);
+    printf("SuperPoder: Carta 1 venceu(%d)\n", Superpoder>Superpoder2);
 
 
 
